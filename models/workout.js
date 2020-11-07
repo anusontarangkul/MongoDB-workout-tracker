0f330c2af4d39fe42a24622c10738cd0647a7b2f
David Anusontarangkul <anusontarangkul.d@gmail.com>
2020-11-07 00:09:42 -0800

    add models

--- models/workout.js
+++ models/workout.js
@@ -1,3 +1,45 @@
 const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+    day: {
+        type: Number,
+        default: Date.now
+    },
+    exercise: {
+        name: {
+            type: String,
+            trim: true,
+            required: "Exercise Type"
+        },
+
+        type: {
+            type: String,
+            trim: true,
+            required: "Exercise Name"
+        },
+        Duration: {
+            type: Number,
+            required: "Duration (minutes)"
+        },
+        weight: {
+            type: Number
+        },
+
+        sets: {
+            type: Number
+        },
+        reps: {
+            type: Number
+        },
+        duration: {
+            type: Number
+        }
+
+    }
+})
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
